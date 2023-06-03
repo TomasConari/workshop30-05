ca90b887fa3f092b796da30c9c182c3c7f33c330
Tomás Contreras <tomasconari@gmail.com>
2023-06-02 23:14:22 -0500

    Fixed search bug, modified style

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import { Graphic } from './components/Graphic';
 import { SearchAndList } from './components/SearchAndList';
 
 const App = () => {
 
-
+  const [graphic, setGraphic] = useState("");
 
   return (
     <div className="App">
       <Graphic />
-      <SearchAndList />
+      <SearchAndList toGraphFunc={setGraphic} />
     </div>
   );
 };
--- src/components/SearchAndList.jsx
+++ src/components/SearchAndList.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { fetchListSearch, fetchList } from "./fetchList";
-import { ListedItems } from "./ListedItems";
 
-export const SearchAndList = () => {
+export const SearchAndList = ({toGraphFunc}) => {
 
     const [list, setList] = useState([]);
 
@@ -24,51 +23,34 @@ export const SearchAndList = () => {
         return(
         <div id="flex2">
             <h1 className="tittle">Control Panel</h1>
-            <input className="input" onChange={fetchSearch} placeholder="Enter your search request..." ></input>
+            <div className="input-container">
+                <input
+                    className="input"
+                    onChange={fetchSearch}
+                    placeholder="   Enter your search request... "
+                />
+            </div>
             <h3 className="borderBottom">Trending cryptos</h3>
             <ul className="flex list">
-                <ListedItems 
-                    keyProp={list[0].id}
-                    srcProp={list[0].image}
-                    nameProp={list[0].name}
-                    symbolProp={list[0].symbol}
-                    priceProp={list[0].price}
-                />
-                <ListedItems 
-                    keyProp={list[1].id}
-                    srcProp={list[1].image}
-                    nameProp={list[1].name}
-                    symbolProp={list[1].symbol}
-                    priceProp={list[1].price}
-                />
-                <ListedItems 
-                    keyProp={list[2].id}
-                    srcProp={list[2].image}
-                    nameProp={list[2].name}
-                    symbolProp={list[2].symbol}
-                    priceProp={list[2].price}
-                />
-                <ListedItems 
-                    keyProp={list[3].id}
-                    srcProp={list[3].image}
-                    nameProp={list[3].name}
-                    symbolProp={list[3].symbol}
-                    priceProp={list[3].price}
-                />
-                <ListedItems 
-                    keyProp={list[4].id}
-                    srcProp={list[4].image}
-                    nameProp={list[4].name}
-                    symbolProp={list[4].symbol}
-                    priceProp={list[4].price}
-                />
-                <ListedItems 
-                    keyProp={list[5].id}
-                    srcProp={list[5].image}
-                    nameProp={list[5].name}
-                    symbolProp={list[5].symbol}
-                    priceProp={list[5].price}
-                />
+                {list.map((el) => (
+                    <li className="listOutsideElement" key={el.id} onClick={toGraphFunc}>
+                        <div className="listElement">
+                            <img
+                                width="40px" 
+                                height="40px" 
+                                alt={`${el.name} icon`}
+                                src={el.image}
+                            />
+                            <div className="nameAndSymbol" style={{display: "flex", flexDirection: "column"}}>
+                                <p>{el.name}</p>
+                                <p className="symbol">{el.symbol}</p>
+                            </div>
+                            <p className="price">
+                                ${el.price}
+                            </p>
+                        </div>
+                    </li>
+                ))}
             </ul>
         </div>
         );
